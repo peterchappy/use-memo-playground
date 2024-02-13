84bf83ae13cb558c7c4ecd4ef3c127f45603d440
Peter Chapman <petechapman@protonmail.com>
2024-02-13 15:08:30 -0500

    feature: support Linearithmic algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import { Constant } from "./Constant";
 import { Linear } from "./Linear";
 import { Quadratic } from "./Quadratic";
+import { Linearithmic } from "./Linearithmic";
 
-const MAX_N = 100_000;
+const MAX_N = 1_000_000;
 
 type PerfType = "linear" | "constant" | "quadratic";
 
@@ -39,33 +40,28 @@ function App() {
   const hundred = data.slice(0, 100);
   const thousand = data.slice(0, 1000);
   const tenThousand = data.slice(0, 10000);
-  const hundredThousand = data;
+  const hundredThousand = data.slice(0, 100_000);
+  const million = data;
 
   return (
     <>
       <div className="perf-table">
-        <div style={{ gridColumn: "span 3" }}>
+        <div style={{ gridColumn: "1 / -1" }}>
           <button onClick={handleRerender}>Re-render</button>
         </div>
-        <div style={{ gridColumn: "span 3" }}>
+        <div style={{ gridColumn: "1 / -1" }}>
           <button onClick={handleDataReset}>Reset</button>
         </div>
         {/* header */}
         <div className="perf-table-item" />
-        <div className="perf-table-item">10 item</div>
-        <div className="perf-table-item">100 items</div>
         <div className="perf-table-item">1,000 items</div>
         <div className="perf-table-item">10,0000 items</div>
         <div className="perf-table-item">100,0000 items</div>
+        <div className="perf-table-item">1,000,0000 items</div>
 
         {/* Constant */}
         <p className="perf-table-item">O(1)</p>
-        <div className="perf-table-item">
-          <Constant data={ten} rerenderCount={triggerRerender} />
-        </div>
-        <div className="perf-table-item">
-          <Constant data={hundred} rerenderCount={triggerRerender} />
-        </div>
+
         <div className="perf-table-item">
           <Constant data={thousand} rerenderCount={triggerRerender} />
         </div>
@@ -75,15 +71,12 @@ function App() {
         <div className="perf-table-item">
           <Constant data={hundredThousand} rerenderCount={triggerRerender} />
         </div>
+        <div className="perf-table-item">
+          <Constant data={million} rerenderCount={triggerRerender} />
+        </div>
 
         {/* Linear */}
         <p className="perf-table-item">O(n)</p>
-        <div className="perf-table-item">
-          <Linear data={ten} rerenderCount={triggerRerender} />
-        </div>
-        <div className="perf-table-item">
-          <Linear data={hundred} rerenderCount={triggerRerender} />
-        </div>
         <div className="perf-table-item">
           <Linear data={thousand} rerenderCount={triggerRerender} />
         </div>
@@ -93,15 +86,30 @@ function App() {
         <div className="perf-table-item">
           <Linear data={hundredThousand} rerenderCount={triggerRerender} />
         </div>
+        <div className="perf-table-item">
+          <Linear data={million} rerenderCount={triggerRerender} />
+        </div>
+
+        <p className="perf-table-item">O(nlogn)</p>
+        <div className="perf-table-item">
+          <Linearithmic data={thousand} rerenderCount={triggerRerender} />
+        </div>
+        <div className="perf-table-item">
+          <Linearithmic data={tenThousand} rerenderCount={triggerRerender} />
+        </div>
+        <div className="perf-table-item">
+          <Linearithmic
+            data={hundredThousand}
+            rerenderCount={triggerRerender}
+          />
+        </div>
+        <div className="perf-table-item">
+          {/* <Linearithmic data={million} rerenderCount={triggerRerender} /> */}
+          <p>N/A</p>
+        </div>
 
         {/* Quadratic */}
         <p className="perf-table-item">O(n^2)</p>
-        <div className="perf-table-item">
-          <Quadratic data={ten} rerenderCount={triggerRerender} />
-        </div>
-        <div className="perf-table-item">
-          <Quadratic data={hundred} rerenderCount={triggerRerender} />
-        </div>
         <div className="perf-table-item">
           <Quadratic data={thousand} rerenderCount={triggerRerender} />
         </div>
@@ -110,6 +118,10 @@ function App() {
         </div>
         <div className="perf-table-item">
           <Quadratic data={hundredThousand} rerenderCount={triggerRerender} />
+        </div>
+        <div className="perf-table-item">
+          {/* <Quadratic data={million} rerenderCount={triggerRerender} /> */}
+          <p>N/A</p>
         </div>
       </div>
     </>
---
+++ src/Linearithmic.tsx
@@ -0,0 +1,66 @@
+import { FC, useMemo } from "react";
+
+type LinearithmicProps = {
+  data: number[];
+  rerenderCount: number;
+};
+
+export const LINEARITHMIC_PERF_NAME = "O(nlogn) useMemo";
+
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]): number[] {
+  let resultArray = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  // Concatenate values into resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // Move to the next element in the left array
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // Move to the next element in the right array
+    }
+  }
+
+  // Concatenate any remaining elements in the left or right array
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+// Exampl
+
+export const Linearithmic: FC<LinearithmicProps> = ({
+  data,
+  rerenderCount,
+}) => {
+  performance.mark("start-memo");
+
+  const displayData = useMemo(() => {
+    return mergeSort(data);
+  }, [data]);
+
+  const name = `${LINEARITHMIC_PERF_NAME}-length-${data.length}`;
+  performance.mark("end-memo");
+  performance.measure(name, "start-memo", "end-memo");
+  const measure = performance.getEntriesByName(name).pop();
+
+  return (
+    <p
+      data-testid={`${rerenderCount} ${displayData.length}`}
+    >{`${measure?.duration}ms`}</p>
+  );
+};
